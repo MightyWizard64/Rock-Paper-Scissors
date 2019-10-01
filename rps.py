c9ed30839cbf797726e6142ce44ca22a29918d02
MightyWizard64 <56050448+MightyWizard64@users.noreply.github.com>
2019-10-01 14:58:09 -0700

    Fixed Product
    -Fixed indentations
    -Fixed Tie variable

--- rps.py
+++ rps.py
@@ -33,29 +33,29 @@ while True:
 	pMove = input("Enter 'r' for rock, 'p' for paper, 's' for scissors, or 'q' for quit: ")
 	# print score
 	printScore()
-	# check if q is entered if so end the game\
+	# check if q is entered if so end the game
 	if pMove == "q":
 		break
 	# get a computer move (random)
 	cMove = random.choice(cMoves)
 	# compare player move with the computer move
 		# player picks rock
-		if pMove == "r":
-			print(pName + " picked Rock")
-			if cMove == "rock":
+	if pMove == "r":
+		print(pName + " picked Rock")
+		if cMove == "rock":
 				print("Computer picks Rock")
 				print("Tie")
-				tie += 1
-			elif cMove == "paper":
+				ties += 1
+		elif cMove == "paper":
 				print("Computer picks Paper")
 				print("Paper covers Rock")
 				cScore += 1
-			else:
+		else:
 				print("Computer picks Scissors")
 				print("Rock breaks Scissors")
 				pScore += 1
 		# player picks paper
-		elif pMove == "p":
+	elif pMove == "p":
 			print(pName + " picked Paper")
 			if cMove == "rock":
 				print("Computer picks Rock")
@@ -64,13 +64,13 @@ while True:
 			elif cMove == "paper":
 				print("Computer picks Paper")
 				print("Tie")
-				tie += 1
+				ties += 1
 			else:
 				print("Computer picks Scissors")
 				print("Scissors cut Paper")
 				cScore += 1
 		# player picks scissors
-		elif pMove == "s":
+	elif pMove == "s":
 			print(pName + " picked Scissors")
 			if cMove == "rock":
 				print("Computer picks Rock")
@@ -83,7 +83,7 @@ while True:
 			else:
 				print("Computer picks Scissors")
 				print("Tie")
-				tie += 1
+				ties += 1
 	# check if pMove is valid
-		else:
+	else:
 			print("That is not an option")
